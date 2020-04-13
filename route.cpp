c3c7681d3dc5e0c0d031fcd1d1f176c9cbcab4fb
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-04-14 03:24:29 +0900

    change branch

--- route.cpp
+++ route.cpp
@@ -95,6 +95,61 @@ class RouteGenerator{
     std::vector<route_pair> route;
     std::vector<route_pair> route_goal;
 };
+template<typename T>
+class accelProfile{
+    public:
+        accelProfile(accelParam &_param):param(_param){}
+        void operator()(std::vector<route_tuple> &_dist){
+          float VEL_INI = std::tuple::get<2>(param);
+          float VEL_FIN = std::tuple::get<3>(param);
+          float total_distance = calDistance();
+          //等速区間に達しない場合の例外処理
+          float accel_section_time = (VEL_MAX - VEL_INI) / ACCEL; 
+          float accel_section_time_pos = 0.5 * (VEL_FIN + TARGET_VEL) * accel_section_time; 
+          float decel_section_time = (VEL_MAX - VEL_FIN) / ACCEL;
+          float decel_section_pos = 0.5 * (VEL_INI + TARGET_VEL) * decel_section_time;
+          float constant_vel_section_pos = total_distance - (accel_section_time_pos + decel_section_pos);
+          float constant_vel_section_time = constant_vel_section_pos / TARGET_VEL;
+        }
+    private:
+        float calDistance(){
+            float temp_distance{};
+            if(isSpline()){
+                for(int i = 0; i < 100; ++i){
+                    float x = [i];
+                    float y = [i];
+                    temp_distance += std::sqrt((x * x) + (y * y));
+                }
+            }else{
+                x_fin == x_ini ? temp_distance = y_fin - y_ini : x_fin - x_ini;
+            }
+            return temp_distance;
+        }
+        bool isSpline(){
+            if(std::tuple::get<2>())
+        }
+        vector<route_pair> dist;
+        std::vector<route_tuple> route;
+        constexpr float TARGET_VEL;
+        constexpr float ACCEL; 
+        constexpr float VEL_MAX;
+        constexpr float VEL_MIN;
+};
+template<typename T>
+class TargetPosition{
+  public:
+    TargetPosition();
+    void operator()(float time){
+      setTime(T);
+    }
+  private:
+    void setQueue(){
+    }
+    void isTargetQueueEmpty(){
+    }
+    accelProfile targetProfile;  
+    RouteGenerator targetRoute;
+};
 enum class Coat{
   red1,
   red2,
